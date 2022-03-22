00957d0d498e3de76cde531baed35037f7922223
joydeep-mukherjee <jmukherj@yorku.ca>
2022-03-22 11:16:45 -0700

    first commit

---
+++ Spring2022_Lab_1/src/Driver.java
@@ -0,0 +1,15 @@
+public class Driver {
+	  public static void main(String args [])
+	  {
+	    Example ex = new Example();
+	    int total;
+
+	    while (true)
+	    {
+	      int num = ex.getNums()[0];
+	      total += num;
+	    }
+
+	    System.out.println(total);
+	  }
+}
